ae506ed5745e413dd15c5a56051ee52209eab541
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-05-20 10:37:51 -0400

    Table Content Generation Function Added

--- Library.js
+++ Library.js
@@ -30,6 +30,17 @@ function generateTableHead(table,data) {
     row.appendChild(th);
   }
 }
+function generateTable(table, data) {
+    for (let element of data) {
+      let row = table.insertRow();
+      for (key in element) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+  }
 let table = document.querySelector("table");
 let data = Object.keys(myLibrary[0]);
 generateTableHead(table,data);
+generateTable(table,myLibrary)
