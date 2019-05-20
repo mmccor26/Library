05daebe7dbad4102488c55b5d828fcf5ca548caa
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-05-20 10:34:53 -0400

    Table Head Generation Added

--- Library.js
+++ Library.js
@@ -1,14 +1,35 @@
-let myLibrary = [];
+let myLibrary = [
+    {Title: "Frankenstein", Pages: 202, Author: "Mary Shelley"},
+    {Title: "A Game of Thrones", Pages: 800,Author:"GRRM"}
+];
+
+
+
 
 function Book(){
-}
-
-function addBookToLibrary(){
     
 }
 
+function addBookToLibrary(book){
+    myLibrary.append(book);
+}
+
 function render(){
+    
     myLibrary.forEach(book =>{
 
     });
 }
+function generateTableHead(table,data) {
+    let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+let table = document.querySelector("table");
+let data = Object.keys(myLibrary[0]);
+generateTableHead(table,data);
