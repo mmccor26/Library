4eeb47586870ed595f3c6077dbc8f34e2cf4214c
mmccor26 <43655058+mmccor26@users.noreply.github.com>
2019-06-13 15:00:50 -0400

    Addition of Delete and Read Button

--- Library.js
+++ Library.js
@@ -17,19 +17,11 @@ function AddBookToLibrary(event){
   const title = document.getElementById("Title").value;
   const author = document.getElementById("Author").value;
   const pages = document.getElementById("Pages").value;
-  console.log(title);
-
-
-  
   myLibrary.push({Title: title,Pages: pages,Author: author});
 
-
-  console.log(myLibrary);
   render();
 
-  
   event.preventDefault();
-  
 }
 
 function showForm(book){
@@ -43,12 +35,15 @@ function showForm(book){
     x.style.visibility= "hidden";
   }
 }
-function generateTableHead(){
-  const table = document.querySelector("table");
-  tHead = table.createTHead();
-  
-
-}
+function generateTableHead(table,data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+}}
 function render(){ 
   const tableBody = document.querySelector("tbody");
 
@@ -64,18 +59,46 @@ function render(){
     const title = document.createElement("td");
     const author = document.createElement("td");
     const pages = document.createElement("td");
+    const readBtn = document.createElement("button")
+    const deleteBtn = document.createElement("button");
+    deleteBtn.style.width = '50px';
+    deleteBtn.style.width = '80px';
+    deleteBtn.innerHTML = "Delete";
+    readBtn.innerHTML = "Unread";
 
     tableBody.appendChild(tableRow);
     
     tableRow.appendChild(title);
     tableRow.appendChild(author);
     tableRow.appendChild(pages);
+    tableRow.appendChild(readBtn);
+    tableRow.appendChild(deleteBtn);
+
     
     title.innerHTML = `${myLibrary[i].Title}`;
     author.innerHTML = `${myLibrary[i].Author}`;
     pages.innerHTML = `${myLibrary[i].Pages}`;
+    
+    deleteBtn.addEventListener('click',(e)=>{
+      deleteBook(i);
+    
+    });
+    readBtn.addEventListener('click',(e)=>{
+      if(readBtn.innerHTML == "Unread"){
+        readBtn.innerHTML = "Read";
+      }
+      else{
+        readBtn.innerHTML = "Unread";
+      }
+    
+    });
   }
 
+}
+function deleteBook(index){
+
+  myLibrary.splice(index,1);
+  render();
 }
 
 
@@ -88,6 +111,7 @@ addBook.addEventListener('click',(e)=>{
   showForm("formDiv");
 
 });
+
 render();
 
 
